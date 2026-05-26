10a7942770aaab4e1299ef688fbad125c3ea380f
ausafahmad-ui <ausaf@example.com>
2026-05-26 18:49:56 +0530

    set

---
+++ 05_Dictionary_Sets/Problem_04.py
@@ -0,0 +1,5 @@
+s={"adeeb"}
+s.add(1)
+s.add('@@')
+print(len(s))
+print(s)
---
+++ 05_Dictionary_Sets/Problem_05.py
@@ -0,0 +1,18 @@
+#-------------Empty Declaration-------------
+#Dict
+d={}
+print(type(d))
+
+#List
+l=[]
+print(type(l))
+
+#Set
+s=set()
+print(type(s))
+
+#Tuple
+t=()
+print(type(t))
+
+#------------- Declaration with Element-------------
